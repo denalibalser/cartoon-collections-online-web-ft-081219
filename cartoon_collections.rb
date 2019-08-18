d7bd84adb64bbfde67a97ac8230ca1363a6e7ff0
Denali Balser <denali.balser167@gmail.com>
2019-08-18 17:48:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.each do |element|
+  array.include? do |element|
     if element.length <= 4
       false
     else
