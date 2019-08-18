aa184df5120f714d517c3df23cae2c8f205d8bd8
Denali Balser <denali.balser167@gmail.com>
2019-08-18 17:57:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,13 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.map do |cheese|
+    if cheese_types.include?(cheese)
+      return cheese
+    else
+      return nil
+    end
+  end  
 end
